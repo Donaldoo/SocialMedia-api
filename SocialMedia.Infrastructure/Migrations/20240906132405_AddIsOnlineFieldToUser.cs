488c505364403eb94c0b62589fcdc70b66adc484
Donaldo <dkumaraku@ritech.co>
2024-09-09 23:09:21 +0200

    update

---
+++ SocialMedia.Infrastructure/Migrations/20240906132405_AddIsOnlineFieldToUser.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMedia.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsOnlineFieldToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Website",
+                table: "User",
+                newName: "Bio");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsOnline",
+                table: "User",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsOnline",
+                table: "User");
+
+            migrationBuilder.RenameColumn(
+                name: "Bio",
+                table: "User",
+                newName: "Website");
+        }
+    }
+}
